f8f31bcb1d60b863d0b5b45dbf33b9cc05e2b1ab
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 17:46:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,13 @@ def countdown(seconds)
 while seconds > 0 do
   puts "#{seconds} SECOND(S)!"
   seconds -= 1
-  sleep 2
 end
 return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0 do
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep 1
+  end
